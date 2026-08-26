4c4f63cde515f6137f90599191584bfba7e393c9
Robson Morcineck <robsongabrielengenharia@gmail.com>
2026-08-26 19:34:46 -0300

    test: adiciona testes do fluxo de check-in

--- src/main/java/com/bths/platform/checkin/CheckInController.java
+++ src/main/java/com/bths/platform/checkin/CheckInController.java
@@ -6,8 +6,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/hospedes")
 public class CheckInController {
